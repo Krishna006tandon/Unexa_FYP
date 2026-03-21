c26b9f8e5156e088956a0696a1b586520ee1c4de
Krishna006tandon <krishnatandon006@gmail.com>
2026-03-22 03:15:35 +0530

    Enable fast refresh for 5-second frontend updates

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,6 +2,26 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-reanimated/plugin'],
+      },
+      development: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['./src'],
+              extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+              alias: {
+                '@': './src',
+              },
+            },
+          ],
+          'react-native-reanimated/plugin',
+        ],
+      },
+    },
     plugins: [
       [
         'module-resolver',
