8a121eafefd22dd045ff2eaa94b7587355c9bf63
Krishna006tandon <krishnatandon006@gmail.com>
2026-03-26 17:00:30 +0530

    fixing email

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -2,17 +2,15 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+    service: 'gmail',
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   const message = {
-    from: `${process.env.EMAIL_FROM} <${process.env.SMTP_USER}>`,
+    from: `"UNEXA SuperApp" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
     text: options.message,
